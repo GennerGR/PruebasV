e7bb9b9875aaaeb92c42594e22b9b71f37f66e69
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-28 14:55:02 -0600

    Initial Commit

---
+++ composeApp/src/commonMain/kotlin/com/gnr/pruebasv/Greeting.kt
@@ -0,0 +1,9 @@
+package com.gnr.pruebasv
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/gnr/pruebasv/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.gnr.pruebasv
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
