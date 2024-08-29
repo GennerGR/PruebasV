d3db6c4a8c99bfdc1931b293799cb6b5dee36e50
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-29 11:06:53 -0600

    BottomBarScreen // Listo.1 ++Home Tab

---
+++ composeApp/src/commonMain/kotlin/com/gnr/pruebasv/BottomBar/BottomBarScreen.kt
@@ -0,0 +1,56 @@
+package com.gnr.pruebasv.BottomBar
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.TabDisposable
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+
+class BottomBarScreen : Screen {
+
+    @Composable
+    override fun Content() {
+        TabNavigator(
+            HomeTab,
+            tabDisposable = {
+                TabDisposable(
+                    it,
+                    listOf(HomeTab)
+                )
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(title = { Text(it.current.options.title) })
+                },
+                bottomBar = {
+                    BottomNavigation {
+                        val tabNavigator = LocalTabNavigator.current
+
+                        BottomNavigationItem(
+                            selected = tabNavigator.current.key == HomeTab.key,
+                            label = { Text(it.current.options.title) },
+                            icon = {
+                                Icon(
+                                    painter = HomeTab.options.icon!!,
+                                    contentDescription = null
+                                )
+                            },
+                            onClick ={tabNavigator.current = HomeTab}
+                        )
+                    }
+                } , content = {}
+            )
+        }
+    }
+}
+
+
+
+
