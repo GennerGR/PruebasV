e61e804037a47f80904af7971492bb8ac3af1ae8
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-29 08:16:35 -0600

    Preparación de navegación básica -- listo

--- composeApp/src/commonMain/kotlin/com/gnr/pruebasv/App.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/pruebasv/App.kt
@@ -1,9 +1,9 @@
 package com.gnr.pruebasv
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,23 +11,43 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import cafe.adriel.voyager.transitions.SlideTransition
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import pruebasv.composeapp.generated.resources.Res
-import pruebasv.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
+        Navigator(screen = MainScreen()) {navigator: Navigator ->
+            SlideTransition(navigator)
+        }
     }
 }
 
+class MainScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = {
+                navigator.push(SecondScreen())
+            }) {
+                Text("SecondScreen", fontSize = 22.sp, color = Color.White)
+            }
+            Spacer(Modifier.height(16.dp))
+            Button(onClick = {
+                navigator.push(BottomBarScreen())
+            }) {
+                Text("BottomBarScreen", fontSize = 22.sp, color = Color.White)
+
+            }
+        }
+    }
+
+}
