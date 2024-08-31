7057d1d75b3424c4e19ff8b12542ab85eb8363b3
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-31 14:26:11 -0600

    nothing

--- composeApp/src/commonMain/kotlin/com/gnr/pruebasv/network/NetworkUtils.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/pruebasv/network/NetworkUtils.kt
@@ -2,11 +2,9 @@ package com.gnr.pruebasv.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.utils.EmptyContent.contentType
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonPrimitive
 
 object NetworkUtils {
     val httpClient = HttpClient {
