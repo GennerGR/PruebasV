4334d7b66a1f49136dad2fc505d5c533ff2e8627
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-06 15:19:40 -0600

    nothing

--- composeApp/src/commonMain/kotlin/com/gnr/pruebasv/api/LoadScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/pruebasv/api/LoadScreen.kt
@@ -1,10 +1,13 @@
 package com.gnr.pruebasv.api
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -57,7 +61,9 @@ class LoadScreen : Screen {
             Spacer(modifier = Modifier.height(16.dp))
             LazyColumn {
                 items(superheroList) { hero ->
-                    Text(hero.name)
+                    Box(modifier = Modifier.background(color = Color.Black).padding(5.dp))
+                    { Text(hero.name, color = Color.White) }
+                    Spacer(modifier = Modifier.height(5.dp))
                 }
             }
             Spacer(modifier = Modifier.height(16.dp))
@@ -72,7 +78,8 @@ class LoadScreen : Screen {
 
     private fun getSuperheroList(superheroName: String, onSuccessResponse: (List<Hero>) -> Unit) {
         if (superheroName.isBlank()) return
-        val url = "https://www.superheroapi.com/api.php/41070f7e0f8585ede295035dc984230c/search/$superheroName"
+        val url =
+            "https://www.superheroapi.com/api.php/41070f7e0f8585ede295035dc984230c/search/$superheroName"
         CoroutineScope(Dispatchers.IO).launch {
             val response = httpClient.get(url).body<ApiResponse>()
             onSuccessResponse(response.results)
